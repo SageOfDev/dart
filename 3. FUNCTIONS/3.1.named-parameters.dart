098938962c2ca6d2698d1b3ea95d5d12b2c283b9
sj <bcj0114@gmail.com>
2023-01-18 11:21:57 +0900

    + 3.3 Optional Positional Parameters

--- 3. FUNCTIONS/3.1.named-parameters.dart
+++ 3. FUNCTIONS/3.1.named-parameters.dart
@@ -1,3 +1,11 @@
+/**
+ * positional parameters: 순서대로 바인딩 하는 것. 여러개의 인수를 객체이 넣지 않고 그냥 쉼표로 연결해서 적어주면 됨
+ * named parameters: 순서 상관없이, key-value 형식으로 바인딩. 인수를 객체로 정의하면 됨.
+ * positional은 2개까지만, 3개부터는 named를 추천함
+ * 
+ * null safety를 피하기 위해선 아래 세 가지 방법을 사용한다.
+ */
+
 String sayHello({
   String? name, 
   int? age, 
---
+++ 3. FUNCTIONS/3.3.optional-positional-parameters.dart
@@ -0,0 +1,10 @@
+/**
+ * positional parameter에서 optional 인수를 정의하고 싶을 때 사용하는 문법
+ * 근데 그냥 Named 추천
+ */
+
+String sayHello(String name, int age, [String country = 'wakanda']) => 'Hello $name, you are $age years old from $country';
+
+void main() {
+  sayHello('J', 12);
+}

2fb0c4fc322a1775903cae2b1bf07ef95ccfded1
sj <bcj0114@gmail.com>
2023-01-18 13:27:46 +0900

    4.4 Recap

---
+++ 4. CLASSES/4.4.recap.dart
@@ -0,0 +1,40 @@
+class Player {
+  final String name;
+  int xp;
+  String team;
+
+  // Named Constructor
+  Player.fromJson(Map<String, dynamic> playerJson)
+      : name = playerJson['name'],
+        xp = playerJson['xp'],
+        team = playerJson['team'];
+
+  void sayHello() {
+    print('Hy myname is $name');
+  }
+}
+
+void main() {
+  var apiData = [
+    {
+      "name": "I",
+      "team": "red",
+      "xp": 0,
+    },
+    {
+      "name": "J",
+      "team": "red",
+      "xp": 0,
+    },
+    {
+      "name": "K",
+      "team": "red",
+      "xp": 0,
+    },
+  ];
+  apiData.forEach((playerJson) {
+    var player = Player.fromJson(playerJson);
+    player.sayHello();
+  });
+}
+`

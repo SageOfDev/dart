33a962812045062e33600f605ef963ba0c163646
sj <bcj0114@gmail.com>
2023-01-24 23:47:53 +0900

    4.8 Inheritance

--- 3. FUNCTIONS/3.3.optional-positional-parameters.dart
+++ 3. FUNCTIONS/3.3.optional-positional-parameters.dart
@@ -7,5 +7,5 @@
 String sayHello(String name, int age, [String country = 'wakanda']) => 'Hello $name, you are $age years old from $country';
 
 void main() {
-  sayHello('J', 12);
+  print(sayHello('J', 12));
 }
